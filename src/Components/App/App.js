e95c56182cbcbec6baa2b6b5f69c0611496382ef
snsaleh1 <snsaleh11@gmail.com>
2019-12-26 21:26:33 -0600

    main-app container created/styled

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-    Hello My World.
-    </>
+    <div className='bills-container'>
+
+    </div>
   );
 }
 
