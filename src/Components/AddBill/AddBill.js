2f680c0ac9d6226c8dae20d9c6cea4e71de0a29a
snsaleh1 <snsaleh11@gmail.com>
2019-12-27 21:07:21 -0600

    addbill file/folder created

---
+++ src/Components/AddBill/AddBill.js
@@ -0,0 +1,10 @@
+import React, { useContext, useState } from 'react';
+import './style.css';
+
+const AddBill = () => {
+    return(
+        <div>Hello from AddBill</div>
+    );
+};
+
+export default AddBill;
