43f96a2736eac722a88d27402b26789d2345dce4
snsaleh1 <snsaleh11@gmail.com>
2019-12-29 14:09:23 -0600

    BillList component files created and imported to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AddBill from '../AddBill/AddBill';
+import BillList from '../BillList/BillList';
 
 const App = () => {
   return (
     <div className='bills-container'>
       <AddBill />
+      <BillList />
     </div>
   );
 }
---
+++ src/Components/BillList/BillList.js
@@ -0,0 +1,11 @@
+import React, { useContext, useState } from 'react';
+import './style.css';
+
+const BillList = () => {
+
+    return (
+        <>Hello from BillList</>
+      );
+};
+
+export default BillList;
